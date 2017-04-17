b935153bc19d34910f33632be6f13ef1b82ad2c0
Owen Davidson <owenmedia@outlook.com>
2017-04-17 16:23:36 -0600

    Added Online Message

--- src/Bot.java
+++ src/Bot.java
@@ -1,13 +1,18 @@
+import java.util.List;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 public class Bot {
 
 	private static JDA jda;
+	
+	private static List<TextChannel> tChannels;
 	
 	public static final String BOT_TOKEN = "MzAzNDAxODE5ODk4NjQyNDMz.C9XnGw.OAddH4VhyFG8FPQ2HAGOz0Qvj1c";
 	
@@ -17,6 +22,11 @@ public class Bot {
 	public static void main(String[] args) {
 		try {
 			jda = new JDABuilder(AccountType.BOT).addListener(new BotListener()).setToken(BOT_TOKEN).buildBlocking();
+			tChannels = jda.getTextChannels();
+			for(int i = 0; i< tChannels.size(); i++){
+				tChannels.get(i).sendMessage("MC is online and ready to respond").complete();
+			}
+			
 		} catch (LoginException | IllegalArgumentException | InterruptedException | RateLimitedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
